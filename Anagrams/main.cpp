6e980d9a04a671e245029d3080abc7f7803470e3
yuyingchua <skymaverick@Skys-MacBook-Pro.local>
2018-09-07 02:41:20 -0700

    Initial Commit

---
+++ Anagrams/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Anagrams
+//
+//  Created by Sky Maverick on 9/7/18.
+//  Copyright © 2018 Yu-Ying Chua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
